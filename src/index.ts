39d382c9aab5cf3622e56bdf5a025b4997d82f29
Adonys Santos <adonysmundotech@gmail.com>
2021-06-07 15:22:46 -0400

    Add pagination

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,55 @@
 import db from "./config/firebase";
 import showDocs from "./helpers/show-docs";
 
-const user = {
-  name: "Adonys",
-  age: 17,
-  title: "Frontend Developer",
-  student: true,
-  salary: 900,
-};
+// const user = {
+//   name: "Adonys",
+//   age: 17,
+//   title: "Frontend Developer",
+//   student: true,
+//   salary: 900,
+// };
 
 // Referencia a la colección users
 const usersRef = db.collection("users");
 
-// Limit 
+// Pagination  //
 
-// Obtiene los 2 primeros documentos de la colección users
-usersRef.limit(2).get().then(showDocs);
+//Btn Next
+const btnNext = document.createElement("button");
+btnNext.innerText = "Next Page";
+document.body.append(btnNext);
 
-// Obtiene los 5 primeros documentos de la colección users
-usersRef.limit(5).get().then(showDocs);
+let firstDocument: any = null;
+let lastDocument: any = null;
+btnNext.addEventListener("click", () => {
+  const query = usersRef.orderBy("name").startAfter(lastDocument);
+
+  query
+    .limit(2)
+    .get()
+    .then((snap) => {
+      firstDocument = snap.docs[0] || null;
+      lastDocument = snap.docs[snap.docs.length - 1] || null;
+      showDocs(snap);
+    });
+});
+
+btnNext.click();
+
+//Btn Previous
+const btnPrevious = document.createElement("button");
+btnPrevious.innerText = "Previous Page";
+document.body.append(btnPrevious);
+
+btnPrevious.addEventListener("click", () => {
+  const query = usersRef.orderBy("name").endBefore(firstDocument);
+
+  query
+    .limit(2)
+    .get()
+    .then((snap) => {
+      firstDocument = snap.docs[0] || null;
+      lastDocument = snap.docs[snap.docs.length - 1] || null;
+      showDocs(snap);
+    });
+});
